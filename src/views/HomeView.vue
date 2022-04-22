cc4097eae5529498652d82b78bcee93d792ba863
andri-irawan79 <iandri647@gmail.com>
2022-04-22 17:45:14 +0800

    Menampilkan menyelesaikan tugas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,17 @@
   <div class="home">
     <HelloWorld msg="Todo Use Graph Query"/>
     <div class="input-group mb-3">
-      <input type="number" placeholder="Masukkan ID" class="form-control" v-model="searchKeyword">
-      <button class="btn btn-success btn-outline-dark text-white">Search</button>
+      <input type="number" placeholder="Masukkan ID" class="form-control" v-model="userId" required>
     </div>
+    <div v-if="$apollo.loading">Loading...</div>
     <div class="items">
-      <ol class="list-group list-group-numbered">
+      <ol v-if="this.userId == ''" class="list-group list-group-numbered">
         <li v-for="item in activity" :key="item.id" class="list-group-item">{{item.activity}}</li>
       </ol>
+      <ol v-else class="list-group">
+        <li class="list-group-item list-none">{{activity_by_pk ? `${activity_by_pk.activity}`: 'Not Found'}}</li>
+      </ol>
     </div>
-
   </div>
 </template>
 
@@ -18,25 +20,44 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import gql from 'graphql-tag'
+// import GET_BY_ID from '../graphql/GetById.gql'
 
 export default {
   name: 'HomeView',
   data() {
     return {
-      searchKeyword: ''
+      userId: '',
+      activity_by_pk: '',
+      activity: ''
+      
     };
   },
   components: {
     HelloWorld
   },
   apollo: {
-    activity: gql`query MyQuery {
-      activity{
-        id
-        activity
-      } 
-    }`,
-  }
+    activity: {
+      query: gql`query MyQuery {
+        activity{
+          id
+          activity
+        } 
+      }`,
+    },
+    activity_by_pk: {
+      query: gql`query MyQuery($userId: Int!) {
+          activity_by_pk(id: $userId) {
+              id
+              activity
+          }
+      }`,
+      variables () {
+        return {
+          userId: parseInt(this.userId),
+        };
+      },
+    }
+  },
 }
 </script>
 
@@ -49,7 +70,12 @@ export default {
   box-sizing: border-box;
 }
 
-ol li {
+ol li, ul li {
   width: 420px;
+  height: 40px;
+}
+
+ul li {
+  text-align: center;
 }
 </style>
